2b0faff58374f2125b7d6cd43d1f29a101b213f5
ks24pon <pom123re0624@gmail.com>
2024-10-20 18:10:19 +0900

    Go&Echo環境構築

---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// 新しいEchoインスタンス生成
+	e := echo.New()
+	// ルートエンドポイントを定義
+	e.GET("/", func(c echo.Context) error {
+		// クライアントに返してレスポンスを返す
+		return c.String(http.StatusOK, "Hello,Pemm！")
+	})
+	// サーバー起動
+	e.Logger.Fatal(e.Start(":8080"))
+}
