c3eafe04002e87649b82ae676ee49d8b4de85d14
ks24pon <pom123re0624@gmail.com>
2024-11-15 17:30:15 +0900

    #14 AutoMigrateの追記

---
+++ backend/models/user.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+	_ "gorm.io/gorm"
+)
+
+type User struct {
+	// 一意の識別子
+	ID uint `json:"id" gorm:"primary_key"`
+	// ユーザーの名前
+	Username string `json:"name" gorm:"size:255;not null"`
+	// メールアドレス
+	Email string `json:"email" gorm:"size:255;unique;not null"`
+	// パスワード
+	Password string `json:"password" gorm:"size:255;not null"`
+	// GORMで自動的に作成日時を保存
+	CreatedAt time.Time `json:"created_at"`
+}
