5af648423d5a0a9cc863dd6974559bdc1a00a786
ks24pon <pom123re0624@gmail.com>
2024-11-26 16:20:18 +0900

    #14 パスワードValidation(8文字以内)

--- backend/models/user.go
+++ backend/models/user.go
@@ -9,11 +9,11 @@ type User struct {
 	// 一意の識別子
 	ID uint `json:"id" gorm:"primary_key"`
 	// ユーザーの名前
-	Username string `json:"name" gorm:"size:255;not null" validate:"min=8"`
+	Username string `json:"name" gorm:"size:255;not null"`
 	// メールアドレス
 	Email string `json:"email" gorm:"size:255;unique;not null"`
 	// パスワード
-	Password string `json:"password" gorm:"size:255;not null"`
+	Password  string    `json:"password" gorm:"size:255;not null" validate:"minlength8"`
 	// GORMで自動的に作成日時を保存
 	CreatedAt time.Time `json:"created_at"`
 }
